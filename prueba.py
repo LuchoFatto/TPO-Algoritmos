b0f2f3397068bb3bfe5aceeb09b8fa6a2bf0e32c
DylanSun03 <dylansun271203@gmail.com>
2026-08-27 16:36:59 -0300

    primeros agregados

---
+++ prueba.py
@@ -0,0 +1,20 @@
+#Usuarios
+def generar_usuarios():
+    pass
+
+
+#Afiliados / médicos
+def cargar_medicos():
+    pass
+
+
+#Farmacias
+def cargar_farmacias():
+    pass
+
+
+#Gestión
+def abrir_menu_gestion():
+    pass
+
+
