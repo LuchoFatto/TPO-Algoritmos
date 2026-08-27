aaaf0c79378a39551a6d00bc959800553ffa5798
LuchoFatto <luciano.fattoni@gmail.com>
2026-08-27 20:40:33 -0300

    Agregado de 3 funciones y correcciones de nombres

--- funciones_iniciales.py
+++ funciones_iniciales.py
@@ -1,47 +1,64 @@
-#Usuarios
-def cargar_usuario():
+# Usuarios
+def alta_usuario():
     pass
+
 
 def eliminar_usuario():
     pass
 
 
-#Afiliados / médicos
-def cargar_medico():
+def verificar_estado_pagos():
     pass
+
+
+def buscar_usuarios():
+    pass
+
+
+# Afiliados / médicos
+def alta_medico():
+    pass
+
 
 def eliminar_medico():
     pass
 
 
-#Farmacias
+# Farmacias
 def cargar_farmacia():
     pass
+
 
 def eliminar_farmacia():
     pass
 
 
-#Menus
+# Menus
 def menu_gestion():
     pass
+
 
 def menu_usuario():
     pass
 
+
 def menu_medico():
     pass
+
 
 def menu_farmacia():
     pass
 
 
-#Turnos
+def menu_alta_usuario():
+    pass
+
+
+# Turnos
 def pedir_turno():
     pass
 
 
-
-#Login y permisos
+# Login y permisos
 def mostrar_menu_segun_rol():
     pass
