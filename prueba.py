469697d94a3b9700f23186d97fdd7f00705fd0cd
FrGuido <frguido@uade.edu.ar>
2026-08-27 18:48:45 -0300

    Nuevas Funciones y cambios de nombres

--- prueba.py
+++ prueba.py
@@ -1,22 +1,39 @@
 #Usuarios
-def generar_usuarios():
+def cargar_usuario():
+    pass
+
+def eliminar_usuario():
     pass
 
 
 #Afiliados / médicos
-def cargar_medicos():
+def cargar_medico():
+    pass
+
+def eliminar_medico():
     pass
 
 
 #Farmacias
-def cargar_farmacias():
+def cargar_farmacia():
+    pass
+
+def eliminar_farmacia():
     pass
 
 
-#Gestión
-def abrir_menu_gestion():
+#Menus
+def menu_gestion():
     pass
 
+def menu_usuario():
+    pass
+
+def menu_medico():
+    pass
+
+def menu_farmacia():
+    pass
 
 
 #Turnos
