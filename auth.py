e9539085d8e27386df17dda6f62ab38d00024085
LuchoFatto <159082463+LuchoFatto@users.noreply.github.com>
2026-09-11 20:56:43 -0300

    Agregado de validaciones y prueba de Docstring

--- auth.py
+++ auth.py
@@ -1,13 +1,31 @@
 import re
 
 
-def validar_email(email) -> bool:
+def validar_email(email):
+    """Validar formato de email.
+
+    Args:
+        email (string): Email ingresado por el usuaio.
+
+    Returns:
+        boolean: Si el email cumple con el formato establecido.
+    """
+
     patron_email = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
 
     return bool(re.match(patron_email, email))
 
 
 def validar_cuit(cuit):
+    """Validar formato de CUIT Argentino.
+
+    Args:
+        cuit (string): CUIT ingresado por usuario.
+
+    Returns:
+        boolean: Si el CUIT cumple con el formato establecido.
+    """
+
     patron_limpiar = re.compile(r"[-\s]")
 
     cuit_limpio = re.sub(patron_limpiar, "", cuit)
@@ -15,13 +33,49 @@ def validar_cuit(cuit):
     return len(cuit_limpio) == 11
 
 
-cuit = input("Ingrese cuit:")
-print(validar_cuit(cuit))
-
-
 def validar_numero_telefono(num_telefono):
-    pass
+    """Validar formato de numero de telefono Argentino.
+
+    Args:
+        num_telefono (string): Numero de telefono ingresado por el usuario.
+
+    Returns:
+        boolean: Si el numero de telefono cumple con el formato establecido.
+    """
+
+    patron_no_digito = re.compile(r"\D")
+    patron_cantidad_numeros = re.compile(r"^11[0-9]{8}$")
+
+    num_limpio = re.sub(patron_no_digito, "", num_telefono)
+
+    return bool(re.match(patron_cantidad_numeros, num_limpio))
 
 
 def validar_contrasenia(contrasenia):
-    pass
+    """Validar formato de contrasenia de telefono Argentino.
+
+    Args:
+        contrasenia (string): Contrasenia ingresada por el usuario.
+
+    Returns:
+        boolean: Si la contrasenia cumple con el formato establecido.
+    """
+
+    patron_contrasenia = re.compile(r"^(?=.*[A-Z])(?=.*\d).{8,}$")
+
+    return bool(re.match(patron_contrasenia, contrasenia))
+
+
+def validacion_letras(texto):
+    """Verificar que un texto contenga letras del abecedario, tildes y eñes.
+
+    Args:
+        texto (string): Texto ingresado por el usuario.
+
+    Returns:
+        boolean: Validar que el texto ingresado no contenga numeros o caracteres especiales.
+    """
+
+    patron_letras = re.compile(r"^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$")
+
+    return bool(re.match(patron_letras, texto))
