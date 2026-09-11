4d909a1eead7c570dd3ae5d03170fc56cf6c93bf
DylanSun03 <dylansun271203@gmail.com>
2026-09-10 21:36:01 -0300

    agrego el Segundo  menu (el menu de Afiliados)

--- menus.py
+++ menus.py
@@ -5,15 +5,15 @@ def mostar_menu_principal():
 
         print()
         print("===================================")
-        print("   SISTEMA DE GESTION OBRA SOCIAL")
+        print("    SISTEMA DE GESTION OBRA SOCIAL")
         print("===================================")
-        print("1. ingresa como afiliado")
-        print("2. ingresa como medico")
-        print("3. ingresa como farmacia")
-        print("4. ingresa como Admin/ Gestion")
+        print("1. Ingresa como afiliado")
+        print("2. Ingresa como medico")
+        print("3. Ingresa como farmacia")
+        print("4. Ingresa como Admin/ Gestion")
         print("5. Salir")
 
-        opcion = input("selecciones una opcion: ")
+        opcion = input("Selecciones una opcion: ")
 
         if opcion == "1":
             print("Ingreso como afiliado")
@@ -26,4 +26,35 @@ def mostar_menu_principal():
         elif opcion == "5":
             print("Saliste!")
         else:
-            print("opcion invalida. ")
+            print("Opcion invalida. ")
+
+
+def mostrar_menu_afiliados():
+    opcion = ""
+
+    while opcion != 5:
+        print()
+        print("===================================")
+        print("    MENU DE AFILIADOS    ")
+        print("===================================")
+        print("1. Ver perfil.")
+        print("2. Ver cartilla.") #detalla que servicios de salud cubre el plan...
+        print("3. Cambiar plan.")
+        print("4. Turnos.")
+        print("5. Volver.")
+
+        opcion = input("Ingrese una opcion: ")
+
+        if opcion == "1":
+            print("MI PERFIL")
+        elif opcion == "2":
+            print("Ver mi cartilla")
+        elif opcion == "3":
+            print("Cambiar plan")
+        elif opcion == "4":
+            print("Ver mis Turnos")
+        elif opcion == "5":
+            print("Volviste al menu principal...")
+        else:
+            print("Opcion invalida")
+
